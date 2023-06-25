201164efa755795087b82a31235ef283d6d58f54
ypli0629 <liyapeng0629@gmail.com>
2023-06-25 16:03:12 +0800

    feat: add user plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,11 +2,41 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
+  {
+    "editorconfig/editorconfig-vim",
+    lazy = false,
+  },
+  {
+    "matze/vim-move",
+    lazy = false,
+  },
+  {
+    "lbrayner/vim-rzip",
+    lazy = false,
+  },
+  {
+    "mbbill/undotree",
+    lazy = false,
+  },
+  {
+    "voldikss/vim-translator",
+    lazy = false,
+  },
+  {
+    "skywind3000/asynctasks.vim",
+    lazy = false,
+  },
+  {
+    "skywind3000/asyncrun.vim",
+    lazy = false,
+  },
+  {
+    "opalmay/vim-smoothie",
+    lazy = false,
+  },
 }

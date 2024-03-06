5631b92c7f749a60b11d3407854026ffb928297a
ypli0629 <liyapeng0629@gmail.com>
2024-03-06 17:02:49 +0800

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,13 +1,13 @@
 return {
   -- Add the community repository of plugin specifications
-  { "AstroNvim/astrocommunity", dev = true },
+  { "AstroNvim/astrocommunity", dev = false },
   -- { dir = "~/projects/astrocommunity" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.utility.transparent-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   -- { import = "astrocommunity.editing-support.auto-save-nvim" },
